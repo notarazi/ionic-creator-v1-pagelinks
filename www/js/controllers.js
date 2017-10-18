191875683a94f8b36f3b12d129e0fd249cb48b81
notarazi <notarazi@gmail.com>
2017-10-18 11:40:34 +0800

    Add $scope.$on('$ionicView.enter', function(){}

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,10 +13,13 @@ function ($scope, $stateParams,$state) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-            alert(
-                angular.toJson($stateParams.param1)+"\n"+
-                angular.toJson($stateParams.param2)+"\n"
-                )
+    $scope.$on('$ionicView.enter', function(){
+      alert(
+        angular.toJson($stateParams.param1) + "\n" +
+        angular.toJson($stateParams.param2) + "\n"
+        )
+      $scope.data=$stateParams;
+  });
 }])
    
 .controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
